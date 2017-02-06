184158a04e276dfe0bd04b50b80fdc929d539601
Gerardo Belot <gbelot2003@hotmail.com>
2017-02-06 08:10:45 -0600

    Cambio de Ruta en custom .js

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $items = User::all();
+        return View('seguridad.usuarios.index', compact('items'));
+    }
 }
--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -42,7 +42,7 @@
         function sendDataToServer(id, ch, selector){
             if(ch === true){
                 var state = 1;
-                $.get('/' + selector + '/' + id + '/' + state)
+                $.get('/pplab/public/' + selector + '/' + id + '/' + state)
                     .done(function(data){
                     toastr.success('Has activado el permiso de <strong style="text-decoration: underline">' + data + '</strong> exitosamente!!');
                 }).fail(function(data){
@@ -53,7 +53,7 @@
 
             } else if(ch === false) {
                 var state = 0;
-                $.get('/' + selector + '/' + id + '/' + state)
+                $.get('/pplab/public/' + selector + '/' + id + '/' + state)
                 .done(function(data){
                     toastr.info('Has desactivado el permiso de <strong style="text-decoration: underline">' + data + '</strong> exitosamente!!');
                 }).fail(function(data){
