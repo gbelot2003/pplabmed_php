b50ae683c80f281c4b170de55be2fb46c019cff6
Gerardo Belot <gbelot2003@hotmail.com>
2017-02-07 08:17:07 -0600

    agregando tablas de agencias

--- routes/web.php
+++ routes/web.php
@@ -16,29 +16,21 @@ Route::get('/', 'PagesController@index' );
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-
 Route::resource('citologias', 'CitologiaController');
 Route::resource('histopatologia', 'HistopatologiaController');
 Route::resource('areas', 'AreaController');
 Route::get('areas/state/{id}/{state}', 'AreaController@state');
-
 Route::resource('firmas', 'FirmasController');
 Route::get('firmas/state/{id}/{state}', 'FirmasController@state');
-
 Route::resource('categorias', 'CategoryController');
 Route::get('categorias/state/{id}/{state}', 'CategoryController@state');
-
 Route::get('permisos', 'PermissionController@index');
 Route::resource('roles', 'RolesController');
-
 Route::resource('usuarios', 'UserController');
 Route::get('usuarios/state/{id}/{state}', 'UserController@state');
-
 Route::resource('gravidad', 'GravidadController');
 Route::get('gravidad/state/{id}/{state}', 'UserController@state');
-
 Route::resource('morfologia', 'MorfologiaController');
 Route::get('morfologia/state/{id}/{state}', 'UserController@state');
-
 Route::resource('topologia', 'TopologiaController');
 Route::get('topologia/state/{id}/{state}', 'UserController@state');
