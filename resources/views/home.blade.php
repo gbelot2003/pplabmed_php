7b4f5bbe56c2e10be6dd6ee3bb1bb26dc5259de1
Gerardo Belot <gbelot2003@hotmail.com>
2017-02-08 11:44:36 -0600

    cambios a login background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            <br>
+            <br>
+            <br>
             <img src="{{ asset('img/Logo.jpg') }}" alt="Logo" width="100%">
         </div>
     </div>
