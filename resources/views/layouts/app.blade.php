74a8968838aef183c7fa7150f4215013c6e1a7cd
Gerardo Belot <gbelot2003@hotmail.com>
2017-02-08 14:23:21 -0600

    cambios de coloración en login y navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,19 @@
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
 
+    <style>
+        <!--
+        .navbar {
+            /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#2d9b95+-1,0e1329+100,278180+100,0e1329+100,2d9b95+100 */
+            background: #2d9b95; /* Old browsers */
+            background: -moz-linear-gradient(top,  #2d9b95 -1%, #0e1329 100%, #278180 100%, #0e1329 100%, #2d9b95 100%); /* FF3.6-15 */
+            background: -webkit-linear-gradient(top,  #2d9b95 -1%,#0e1329 100%,#278180 100%,#0e1329 100%,#2d9b95 100%); /* Chrome10-25,Safari5.1-6 */
+            background: linear-gradient(to bottom,  #2d9b95 -1%,#0e1329 100%,#278180 100%,#0e1329 100%,#2d9b95 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2d9b95', endColorstr='#2d9b95',GradientType=0 ); /* IE6-9 */
+        }
+        -->
+    </style>
+
     {{--<script src="{{ asset('js/app.js') }}"></script>--}}
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -74,8 +74,38 @@
                         <h1 class="logo-caption"><span class="tweak">PPlab</span>med</h1>
                     </div><!-- /.logo -->
                     <div class="controls">
-                        <input type="text" name="username" placeholder="username" class="form-control" />
-                        <input type="password" name="password" placeholder="Password" class="form-control" />
+                        <div class="row">
+                            <div class="col-md-10 col-md-push-1">
+
+                                <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                                    <label for="username" class="col-md-2 control-label">Usuario</label>
+                                    <div class="col-md-12">
+                                        <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
+
+                                        @if ($errors->has('username'))
+                                            <span class="help-block">
+                                        <strong>{{ $errors->first('username') }}</strong>
+                                    </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group {{ $errors->has('password') ? ' has-error' : '' }}">
+
+                                    <label for="password" class="col-md-2 control-label">Password</label>
+                                    <div class="col-md-12">
+                                        <input id="password" type="password" class="form-control" name="password" required>
+                                        @if ($errors->has('password'))
+                                            <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
                         <button type="submit" class="btn btn-default btn-block btn-custom">Login</button>
                     </div><!-- /.controls -->
                 </div><!-- /#login-box -->
@@ -83,18 +113,37 @@
         </div><!-- /.container -->
     </div>
     <style>
-        body{
+        body {
             margin: 0;
             padding: 0;
             overflow: hidden;
             background: #2d9b95;
             background: -moz-radial-gradient(center, ellipse cover, #2d9b95 0%, #0e1329 100%);
-            background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%,#2d9b95), color-stop(100%,#0e1329));
-            background: -webkit-radial-gradient(center, ellipse cover, #2d9b95 0%,#0e1329 100%);
-            background: -o-radial-gradient(center, ellipse cover, #2d9b95 0%,#0e1329 100%);
-            background: -ms-radial-gradient(center, ellipse cover, #2d9b95 0%,#0e1329 100%);
-            background: radial-gradient(ellipse at center, #2d9b95 0%,#0e1329 100%);
-            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2d9b95', endColorstr='#0e1329',GradientType=1 );
+            background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, #2d9b95), color-stop(100%, #0e1329));
+            background: -webkit-radial-gradient(center, ellipse cover, #2d9b95 0%, #0e1329 100%);
+            background: -o-radial-gradient(center, ellipse cover, #2d9b95 0%, #0e1329 100%);
+            background: -ms-radial-gradient(center, ellipse cover, #2d9b95 0%, #0e1329 100%);
+            background: radial-gradient(ellipse at center, #2d9b95 0%, #0e1329 90%);
+            filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#2d9b95', endColorstr='#0e1329', GradientType=1
+            );
+        }
+
+
+
+
+
+
+        label{
+            color: white;
+        }
+
+        .help-block {
+            font-size: 1rem;
+        }
+
+
+
+
     </style>
 @stop
 @section('styles')
