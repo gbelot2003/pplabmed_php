be6379ccfadb9795832374963814226191d620de
gbelot <gbelot2003@hotmail.com>
2017-03-28 21:16:32 -0600

    validando FacturasApi

--- app/Http/Controllers/FactutasApiController.php
+++ app/Http/Controllers/FactutasApiController.php
@@ -34,10 +34,10 @@ class FactutasApiController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return string
      */
-    public function store(Request $request)
+    public function store(FacturasValidate $request)
     {
 
-        /*$request['fecha_nacimiento'] = $this->setFecha($request->get('fecha_nacimiento'));
+        $request['fecha_nacimiento'] = $this->setFecha($request->get('fecha_nacimiento'));
         $request['edad'] = $this->setEdad($request->get('edad'));
         $factura = Factura::create($request->all());
         $examenes = $factura->examenes;
@@ -45,9 +45,8 @@ class FactutasApiController extends Controller
             $factura->examenes->create($request->all());
         }
 
-        $result = '200';*/
-        dd($request->all());
-        return $request->all();
+        $result = '200';
+        return $result;
     }
 
     /**
