1c8a82458f4768dd49583a698622ab7ac937a0bd
Gerardo <gbelot@sinergiacala.net>
2017-05-08 11:55:13 -0600

    Refactoring Plantillas

--- app/Http/Controllers/PlantillasController.php
+++ app/Http/Controllers/PlantillasController.php
@@ -15,17 +15,27 @@ class PlantillasController extends Controller
         $this->middleware('ManageTemplates');
     }
 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $items = Plantilla::orderBy('id', 'DESC')->paginate(10);
         return View('plantillas.index', compact('items'));
     }
 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
         return View('plantillas.create', compact('items'));
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
     {
         $items = new Plantilla();
@@ -39,12 +49,22 @@ class PlantillasController extends Controller
         return redirect()->to(action('PlantillasController@index'));
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function edit(Request $request, $id)
     {
         $item = Plantilla::findOrFail($id);
         return View('plantillas.edit', compact('item'));
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function update(Request $request, $id)
     {
         $item = Plantilla::findOrFail($id);
@@ -70,12 +90,20 @@ class PlantillasController extends Controller
         return $item->name;
     }
 
+    /**
+     * @param $type
+     * @return mixed
+     */
     public function listado($type)
     {
         $item = Plantilla::where('type', $type)->get();
         return $item;
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function getPlantilla($id)
     {
         $item = Plantilla::findOrFail($id);
