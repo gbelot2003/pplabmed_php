5fe29f704e623bf86b1cc7b6f216354f957f0a6a
Gerardo Belot <gbelot2003@hotmail.com>
2017-06-11 20:37:55 -0600

    Configurando archivos de script para imagenes form

---
+++ resources/assets/js/scripts/image-form.js
@@ -0,0 +1,77 @@
+(function(){
+    $('.imageCanvas').each(function (ind, val){
+        var x = 0, y = 0;
+
+        let image   = $(this).find('.image');
+        let btnPlus = $(this).find('#changePlus');
+        let btnLess = $(this).find('#changeLess');
+        let btnSave = $(this).find('#changeSave');
+        let crf = $('input[name="_token"]').val();
+        let del = $(this).find('.delete');
+
+        let id = image.prop('id');
+        let name = image.prop('alt');
+        console.log('id-' + id);
+
+
+        let classList = image.attr('class');
+        let arr = classList.split(' ');
+        let className = '.'+arr[arr.length-1];
+        console.log(className);
+
+        $(btnPlus).click(function (e) {
+            e.preventDefault();
+            btnSave.show();
+            y = (x + 5);
+
+            Caman(className, function () {
+                this.brightness(y);
+                this.render();
+            });
+        });
+
+        $(btnLess).click(function (e) {
+            e.preventDefault();
+            btnSave.show();
+            y = (x - 5);
+
+            Caman(className, function () {
+                this.brightness(y);
+                this.render();
+            });
+        });
+
+        $(btnSave).click(function(){
+            let canvas = $(className);
+            let dataUrl = canvas.get(0).toDataURL();
+            let linkId = $('input[name="link_id"]').val();
+
+            $.ajax({
+                type: "put",
+                url: "/histo/images/update/" + id,
+                data: {
+                    '_token': crf,
+                    'images': dataUrl,
+                    'link_id': linkId,
+                    'name': name
+                }
+            }).done(function(response) {
+                btnSave.hide();
+                console.log(response);
+            });
+        });
+
+        $(del).on('click',function(e){
+            e.preventDefault();
+            $(this).closest("div").remove();
+            $.ajax({
+                type: "get",
+                url:"/histo/images/delete/" + id
+            }).done(function(res){
+                $(this).closest("div").remove();
+                console.log('read')
+            })
+        })
+
+    });
+})(jQuery);
--- resources/views/resultados/histopatologia/image/edit.blade.php
+++ resources/views/resultados/histopatologia/image/edit.blade.php
@@ -14,26 +14,37 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="text-muted pull-right">
-                            <a href="/histopatologia/{{ $item->histo->id }}/edit" class="btn btn-default">Regresar a Formulario</a>
+                    <form action="">
+                        <div class="panel-heading">
+                            <div class="text-muted pull-right">
+                                <a href="/histopatologia/{{ $item->histo->id }}/edit" class="btn btn-default">Regresar a Formulario</a>
+                            </div>
+                            <h4>Edición de Imagenes</h4>
                         </div>
-                        <h4>Edición de Imagenes</h4>
-                    </div>
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-md-3"></div>
-                            <div class="col-md-9" style="background: grey; padding: 5px">
-                                <div class="image text-center">
-                                    <img src="/img/histo/{{ $item->image_url }}" alt="{{ $item->image_url }}">
+
+                        <div class="panel-body">
+
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <h4>Editor</h4>
                                 </div>
-                                <div class="form-group">
-                                    <br>
-                                    <textarea name="descricion" class="form-control textarea" rows="2"></textarea>
+
+                                <div class="col-md-9" style="background: grey; padding: 5px">
+                                    <div class="image text-center">
+                                        <img name="image" src="/img/histo/{{ $item->image_url }}" alt="{{ $item->image_url }}">
+                                    </div>
+                                    <div class="form-group">
+                                        <br>
+                                        <textarea name="descricion" class="form-control textarea" rows="2"></textarea>
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
-                    </div>
+                        <div class="panel-footer">
+
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
