c929dadcf5789218024adba381f8efb91f767499
Gerardo <gbelot@sinergiacala.net>
2017-06-13 15:38:41 -0600

    cambios menores

--- app/Http/Controllers/CitologiaController.php
+++ app/Http/Controllers/CitologiaController.php
@@ -96,8 +96,8 @@ CitologiaController extends Controller
     public function update(CitologiaValidate $request, $id)
     {
         $cito = Citologia::findOrFail($id);
-        $cito->mm = isset($request['deteccion_cancer']) ? $request['deteccion_cancer'] = 1 : $request['deteccion_cancer'] = 0;
-        $cito->mm = isset($request['indice_maduracion']) ? $request['indice_maduracion'] = 1 : $request['indice_maduracion'] = 0;
+        $cito->deteccion_cancer = isset($request['deteccion_cancer']) ? $request['deteccion_cancer'] = 1 : $request['deteccion_cancer'] = 0;
+        $cito->indice_maduracion = isset($request['indice_maduracion']) ? $request['indice_maduracion'] = 1 : $request['indice_maduracion'] = 0;
         $cito->mm = isset($request['mm']) ? $request['mm'] = 1 : $request['mm'] = 0;
         $cito->update($request->all());
         $cito->facturas->update($request->all());
