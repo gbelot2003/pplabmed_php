8b2d04e5fb9490a9e6c5f474bf12b40473c19bc2
Gerardo Belot <gbelot2003@hotmail.com>
2017-06-15 11:50:14 -0600

    reprocesando las facturas invalidadas

--- app/Http/Controllers/FactutasApiController.php
+++ app/Http/Controllers/FactutasApiController.php
@@ -20,6 +20,14 @@ class FactutasApiController extends Controller
     {
         $factHelp = new FacturasApiHeper();
 
+
+        if ($request->has('status')){
+            $status = $request->get('status');
+            if ($status != 'Valida'){
+                return $this->rewriteFile($request);
+            }
+        }
+
         if ($request->has('fecha_nacimiento')){
             $fecha_nac = new DateHelper($request->get('fecha_nacimiento'));
             $request['fecha_nacimiento'] = $fecha_nac->getDate();
@@ -31,7 +39,19 @@ class FactutasApiController extends Controller
             $factHelp->saveExamenes($request->get('examen'), $factura->num_factura);
         }
 
+        return '200';
+    }
 
+    /**
+     * @param Request $request
+     * @return string
+     */
+    protected function rewriteFile(Request $request): string
+    {
+        $fac_num = $request->get('num_factura');
+        $factura = Factura::where('num_factura', $fac_num)->first();
+        $factura->status = 'Anulada';
+        $factura->save();
         return '200';
     }
 
--- tests/Feature/FacturasApiControllerTest.php
+++ tests/Feature/FacturasApiControllerTest.php
@@ -30,6 +30,25 @@ class FacturasApiControllerTest extends TestCase
             'direccion_entrega_sede' => 'HOSPITAL MILITAR',
             'medico' => 'Dr. Jorge Rodriguez',
             'sexo' => 'F',
+            'status' => 'Valida'
+            /*'examen' => array(
+                ['codigo_examen' => 10260, 'nombre_examen' => 'Marcador Tumoral En Biopsia - Cd-30'],
+                ['codigo_examen' => 10261, 'nombre_examen' => 'Marcador Tumoral Antigeno Epitelial De Membrana'],
+                ['codigo_examen' => 10262, 'nombre_examen' => 'Marcador Tumoral En Biopsia']
+            )*/
+
+        ];
+
+        $data2 = [
+            'num_factura' => '5005963',
+            'num_cedula' => '0801198813342',
+            'nombre_completo_cliente' => 'Edith Marisol Alvarez Mena',
+            'fecha_nacimiento' => '',
+            'correo' => 'edith.m.mena@gmail.com',
+            'direccion_entrega_sede' => 'HOSPITAL MILITAR',
+            'medico' => 'Dr. Jorge Rodriguez',
+            'sexo' => 'F',
+            'status' => 'Valida'
             /*'examen' => array(
                 ['codigo_examen' => 10260, 'nombre_examen' => 'Marcador Tumoral En Biopsia - Cd-30'],
                 ['codigo_examen' => 10261, 'nombre_examen' => 'Marcador Tumoral Antigeno Epitelial De Membrana'],
@@ -40,5 +59,44 @@ class FacturasApiControllerTest extends TestCase
 
         $response = $this->call('POST', '/api/facturas', $data);
         $response->assertStatus(200);
+
+        $response2 = $this->call('POST', '/api/facturas', $data2);
+        $response2->assertStatus(500);
+    }
+
+    /**
+     * @test
+     */
+    public function a_factura_reedit_a_invalidated_register()
+    {
+        $data = [
+            'num_factura' => '5005963',
+            'num_cedula' => '0801198813342',
+            'nombre_completo_cliente' => 'Edith Marisol Alvarez Mena',
+            'fecha_nacimiento' => '',
+            'correo' => 'edith.m.mena@gmail.com',
+            'direccion_entrega_sede' => 'HOSPITAL MILITAR',
+            'medico' => 'Dr. Jorge Rodriguez',
+            'sexo' => 'F',
+            'status' => 'Valida'
+        ];
+
+        $data2 = [
+            'num_factura' => '5005963',
+            'num_cedula' => '0801198813342',
+            'nombre_completo_cliente' => 'Edith Marisol Alvarez Mena',
+            'fecha_nacimiento' => '',
+            'correo' => 'edith.m.mena@gmail.com',
+            'direccion_entrega_sede' => 'HOSPITAL MILITAR',
+            'medico' => 'Dr. Jorge Rodriguez',
+            'sexo' => 'F',
+            'status' => 'Invalida'
+        ];
+
+        $response = $this->call('POST', '/api/facturas', $data);
+        $response->assertStatus(200);
+
+        $response2 = $this->call('POST', '/api/facturas', $data2);
+        $response2->assertStatus(200);
     }
 }
