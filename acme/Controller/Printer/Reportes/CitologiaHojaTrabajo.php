8197a21fa187571962b36aa55effa903a070b17b
Gerardo Belot <gbelot2003@hotmail.com>
2017-06-19 16:27:20 -0600

    fix dpdf Citologia

--- acme/Controller/Printer/Reportes/CitologiaHojaTrabajo.php
+++ acme/Controller/Printer/Reportes/CitologiaHojaTrabajo.php
@@ -27,31 +27,29 @@ class CitologiaHojaTrabajo
 
         $pdf->AddPage();
 
-        $pdf->SetAutoPageBreak(false, 0);
+        $pdf->SetAutoPageBreak(true, 30);
 
         $pdf->SetFont('Arial', '', 8);
-
         foreach ($data as $rows){
 
             /**
              * No Factura
              */
-            $pdf->MultiCell('20  ', '5', $this->ConvertCharacters->convert($rows->num_factura), 1, 'L');
+            $pdf->Cell('20  ', '10', $rows->num_factura, 1, '', 'L');
             $x = $pdf->GetX();
             $y = $pdf->GetY();
+            /**
+             * Nombre Paciente
+             */
+            $pdf->MultiCell('45', '5', $this->ConvertCharacters->convert($rows->nombre_completo_cliente), 1, 'L');
             $pdf->SetXY($x , $y+5);
             /**
              * Direccion sede
              */
             $pdf->SetFont('Arial', '', 5.5);
 
-            $pdf->MultiCell('20', '5', $this->ConvertCharacters->convert($rows->direccion_entrega_sede), 1, 'L');
-            $pdf->SetXY($x + 20 , $y);
-            /**
-             * Nombre Paciente
-             */
-            $pdf->Cell('45', '10', $this->ConvertCharacters->convert($rows->nombre_completo_cliente), 1, 'L');
-
+            $pdf->MultiCell('45', '5', $this->ConvertCharacters->convert($rows->direccion_entrega_sede), 1, 'L');
+            $pdf->SetXY($x + 45 , $y);
 
             /**
              * Edad
@@ -76,13 +74,13 @@ class CitologiaHojaTrabajo
 
             $pdf->MultiCell('45', 10, $this->ConvertCharacters->convert(substr($rows->examen['nombre_examen'], 0,32)) , 'B', 'L', '');
 
-            $pdf->SetXY($x + 164 , $y);
+            $pdf->SetXY($x + 145 , $y);
             /**
              * Informe
              */
             $pdf->SetFont('Arial', '', 7);
             $pdf->Cell('30', '10', $this->checkSerial($rows->created_at->format('Y'), $rows->serial),1, 0, 'C', 0, '');
-            $pdf->ln();
+            $pdf->ln(10 );
         }
 
         return $pdf->Output();
