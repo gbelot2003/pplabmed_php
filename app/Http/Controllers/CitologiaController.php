ca4d55f42248c0709185b5a9460461f0c6936aa5
Gerardo Belot <gbelot2003@hotmail.com>
2017-06-20 14:35:50 -0600

    citologia

--- app/Http/Controllers/CitologiaController.php
+++ app/Http/Controllers/CitologiaController.php
@@ -106,11 +106,6 @@ CitologiaController extends Controller
         $previous = Citologia::where('id', '<', $item->id)->max('id');
         $next = Citologia::where('id', '>', $item->id)->min('id');
 
-
-        $total = Citologia::all()->count();
-        $first = $total->first();
-        $last = $total->last();
-
         $now = date("Y-m-d");
         $bdate = Carbon::createFromFormat('Y-m-d', $now)->startOfDay();
         $edate = Carbon::createFromFormat('Y-m-d', $now)->endOfDay();
