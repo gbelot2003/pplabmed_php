5fa59a078a697f49e4fa012d987109538d11e93c
Gerardo <gbelot@sinergiacala.net>
2017-07-04 16:51:23 -0600

    cambiando tipo de letra en patología a Arial

--- app/Http/Controllers/FacturasController.php
+++ app/Http/Controllers/FacturasController.php
@@ -71,6 +71,7 @@ class FacturasController extends Controller
             'created_at'
         ])
             ->limit(1500)
+            ->orderBy('num_factura', 'ASC')
             ->get();
         return Datatables::of($facturas)
             ->addColumn('href', function($facturas){
