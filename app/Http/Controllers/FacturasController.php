6e112ac2e8afb92db2f0c251c4e63ffbbafe516a
Gerardo <gbelot@sinergiacala.net>
2017-07-04 17:00:18 -0600

    FacturasContoller

--- app/Http/Controllers/FacturasController.php
+++ app/Http/Controllers/FacturasController.php
@@ -72,6 +72,7 @@ class FacturasController extends Controller
             'created_at'
         ])
             ->orderBy('num_factura', 'ASC')
+            ->limit(1500)
             ->get();
         return Datatables::of($facturas)
             ->addColumn('href', function($facturas){
