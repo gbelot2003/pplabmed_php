8463e1740714a6f6046d27d6d0f135e711926c01
Gerardo Belot <gbelot2003@hotmail.com>
2017-07-13 15:51:47 -0600

    correción de espacios nueva papeleria 4

--- acme/Controller/Printer/Bases/PDF.php
+++ acme/Controller/Printer/Bases/PDF.php
@@ -229,7 +229,7 @@ class PDF extends baseFpdf
         }
 
         if($this->last_page_flag == true){
-            $this->SetY(-25);
+            $this->SetY(-30);
             $this->Cell(45, 5, "Fecha de Informe:" , 0, '');
             $this->SetFont('Arial', 'B', 11);
 
@@ -260,7 +260,7 @@ class PDF extends baseFpdf
         }
 
         // Position at 1.5 cm from bottom
-        $this->SetY(-15);
+        $this->SetY(-20);
         // Arial italic 8
         $this->SetFont('Arial','I',8);
         // Page number
