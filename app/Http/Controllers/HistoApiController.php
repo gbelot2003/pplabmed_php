85c5526832b646465110fdec275cca918b0c4cc1
Gerardo Belot <gbelot2003@hotmail.com>
2017-08-01 09:05:47 -0600

    Cambio de estatus en subida de imagenes via movil

--- app/Http/Controllers/HistoApiController.php
+++ app/Http/Controllers/HistoApiController.php
@@ -31,7 +31,7 @@ class HistoApiController extends Controller
                     'link_id' => $request->get('link_id')
                 ]);
 
-                return $images->image_url;
+                return response()->setStatusCode(200);
             }
         }
     }
