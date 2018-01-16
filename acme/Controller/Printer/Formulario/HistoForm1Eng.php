3f56b16bff7fc464d776cd790aaf7ceb7d4ffdc1
Gerardo Belot <gbelot2003@hotmail.com>
2018-01-16 09:41:05 -0600

    Cambiando fecha de histoFormEng a fecha_informe para evitar omisiones en formulario

--- acme/Controller/Printer/Formulario/HistoForm1Eng.php
+++ acme/Controller/Printer/Formulario/HistoForm1Eng.php
@@ -32,7 +32,7 @@ class HistoForm1Eng
 
     public function printPdfHitoReport(HistopatologiasEng $data)
     {
-        $ftitle = $data->serial . "-" . $data->fecha_biopcia->format('Y');
+        $ftitle = $data->serial . "-" . $data->fecha_informe->format('Y');
         $pdf = new PDFENG($orientation = 'P', $unit = 'mm', $size = 'Letter', $ftitle = $ftitle, $data);
         $pdf->SetHeaderMargin(28);
         $pdf->setFooterMargin(20);
