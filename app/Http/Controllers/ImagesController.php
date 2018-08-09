9aca166cb60f45fba2c75c9c586a4273c5eb095c
gbelot2003 <gbelot2003@hotmail.com>
2018-08-09 12:46:56 -0600

    fix error de upload images

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -31,7 +31,7 @@ class ImagesController extends Controller
             $helpers->getImagesForUpload($request);
         }
 
-        return redirect()->to(action('HistopatologiaController@edit', $histo->factura_id));
+        return redirect()->to(action('HistopatologiaController@edit', $histo->id));
     }
 
     /**
@@ -61,7 +61,7 @@ class ImagesController extends Controller
         }
 
         $images->save();
-        return redirect()->to('/histopatologia/' .$images->histo->factura_id ."/edit");
+        return redirect()->to('/histopatologia/' .$images->histo->id ."/edit");
 
     }
 
@@ -70,7 +70,7 @@ class ImagesController extends Controller
         $images = Image::findOrfail($id);
         $images->delete();
         flash('La imágen a sido retirada exitosamente', 'success')->important();
-        return redirect()->to('/histopatologia/' .$images->histo->factura_id ."/edit");
+        return redirect()->to('/histopatologia/' .$images->histo->id ."/edit");
     }
 
 
