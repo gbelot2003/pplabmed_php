4c85092eab5e8208b550a6f2865f0dae4de65c6b
gbelot2003 <gbelot2003@hotmail.com>
2019-04-22 08:52:55 -0600

    <fix> error en ingreso de variable

--- acme/Helpers/PdfStringConversor.php
+++ acme/Helpers/PdfStringConversor.php
@@ -10,7 +10,7 @@ class PdfStringConversor
         $encoding = mb_detect_encoding($string, mb_detect_order(), false);
 
         if ($encoding == "UTF-8") {
-            $string = mb_convert_encoding($text, 'UTF-8', 'UTF-8');
+            $string = mb_convert_encoding($string, 'UTF-8', 'UTF-8');
         }
 
         $out = iconv(mb_detect_encoding($string, mb_detect_order(), false), "UTF-8//IGNORE", $text);
